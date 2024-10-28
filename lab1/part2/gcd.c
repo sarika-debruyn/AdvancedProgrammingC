ac9233044a04d9f35baaf19258772eeed432ca58
sarika-debruyn <156474534+sarika-debruyn@users.noreply.github.com>
2024-10-28 19:34:48 -0400

    Create gcd.c

---
+++ lab1/part2/gcd.c
@@ -0,0 +1,9 @@
+// Euclidean algorithm for finding the greatest common denominator (GCD) of two variables
+int gcd(int x, int y){
+	while (y != 0) {
+		int z = y;
+		y = x % y;
+		x = z;
+	}
+	return x;
+}
