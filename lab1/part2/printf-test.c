b821e18eee15e303994e7ad98eb81f063851cf56
sarika-debruyn <156474534+sarika-debruyn@users.noreply.github.com>
2024-10-28 19:36:15 -0400

    Create printf-test.c

---
+++ lab1/part2/printf-test.c
@@ -0,0 +1,13 @@
+#use to test main code
+#include <stdio.h>
+
+int main()
+{
+    int x = -1;
+
+    printf("printf-ing -1 as %%d: %d\n", x);
+    printf("printf-ing -1 as %%u: %u\n", x);
+    printf("printf-ing -1 as %%x: %x\n", x);
+
+    return 0;
+}
